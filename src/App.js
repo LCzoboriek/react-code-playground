5556df8a4614aff5b85beeba97a005e1fb33b1be
Luke Czoboriek <lc169782@protonmail.com>
2022-09-05 14:54:56 +0100

    Created React Codig Playground just to try out different hooks, components and practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = React.useState(0);
+
+  const handleIncrease = () => {
+    console.log("Increase");
+    setCount(count + 1);
+  };
+
+  const handleDecrease = () => {
+    console.log("Decrease");
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Count: {count}
+      <button type="button" onClick={handleIncrease}>
+        Increase
+      </button>
+      <button type="button" onClick={handleDecrease}>
+        Decrease
+      </button>
     </div>
   );
-}
+};
 
 export default App;
